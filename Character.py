85bcf1fa1a0f3a6c4db97d85e9fd6005f40fa5dc
KimSeongtae <seongtae07@gmail.com>
2021-10-20 22:13:23 +0900

    점프 수정

--- Character.py
+++ Character.py
@@ -1,6 +1,7 @@
 from pico2d import *
 SCREENW = 1280
-
+mariosizex = 55
+mariosizey = 55
 startx = 0
 
 class Mario:
@@ -8,25 +9,26 @@ class Mario:
         self.image = load_image('Mario.png')
         self.frame = 1
         self.start = 0
-        self.x = 300; self.y = 90
+        self.x = 300; self.y = 125
         self.startx = 0
         self.marioRight = True
         self.marioLeft = True
+        self.jumpdir = False; self.jumpstart = 0; self.jumping = False
     def right_move_draw(self):
         self.start = 32
-        self.image.clip_draw(self.start + self.frame * 15, 34, 16, 16, self.x, self.y, 48, 48)
+        self.image.clip_draw(self.start + self.frame * 15, 34, 16, 16, self.x, self.y, mariosizex, mariosizey)
 
     def left_move_draw(self):
         self.start = 32
-        self.image.clip_draw(self.start + self.frame * 15, 34, 16, 16, self.x, self.y, 48, 48)
+        self.image.clip_draw(self.start + self.frame * 15, 34, 16, 16, self.x, self.y, mariosizex, mariosizey)
 
     def idle_draw(self):
         self.start = 0
-        self.image.clip_draw(self.start, 34, 16, 16, self.x, self.y, 48, 48)
+        self.image.clip_draw(self.start, 34, 16, 16, self.x, self.y, mariosizex, mariosizey)
 
     def jump_draw(self):
         self.start = 16 * 7
-        self.image.clip_draw(self.start, 34, 16, 16, self.x, self.y, 48, 48)
+        self.image.clip_draw(self.start, 34, 16, 16, self.x, self.y, mariosizex, mariosizey)
 
     def move_right(self):
         global startx
@@ -35,9 +37,9 @@ class Mario:
         else:
             self.marioRight = True
         if self.marioRight:
-            self.x += 5
+            self.x += 10
         else:
-            self.startx += 5
+            self.startx += 10
             startx = self.startx
 
         self.frame = (self.frame + 1) % 4
@@ -51,12 +53,71 @@ class Mario:
             startx = 0
 
         if self.marioLeft:
-            self.x -= 5
+            self.x -= 10
         else:
-            self.startx -=5
+            self.startx -= 10
             startx = self.startx
 
         self.frame = (self.frame + 1) % 4
 
+    def jump_left(self):
+        if self.jumping == False and self.jumpstart == 0:
+            self.jumpstart = self.y
+            self.jumpdir = True
+            self.jumping = True
+        else:
+            if self.jumping:
+                if self.jumpdir:
+                    self.y += 25
+                    self.x -= 10
+                    if self.y - self.jumpstart >= 105:
+                        self.jumpdir = False
+                else:
+                    self.y -= 25
+                    self.x -= 10
+                    if self.y <= self.jumpstart:
+                        self.y = self.jumpstart
+                        self.jumpstart = 0
+                        self.jumping = False
+        return self.jumping
+
+    def jump_right(self):
+        if self.jumping == False and self.jumpstart == 0:
+            self.jumpstart = self.y
+            self.jumpdir = True
+            self.jumping = True
+        else:
+            if self.jumping:
+                if self.jumpdir:
+                    self.y += 25
+                    self.x += 10
+                    if self.y - self.jumpstart >= 105:
+                        self.jumpdir = False
+                else:
+                    self.y -= 25
+                    self.x += 10
+                    if self.y <= self.jumpstart:
+                        self.y = self.jumpstart
+                        self.jumpstart = 0
+                        self.jumping = False
+        return self.jumping
+
+    def idle_jump(self):
+        if self.jumpstart == 0 and self.jumping == False:
+            self.jumpstart = self.y
+            self.jumpdir = True
+            self.jumping = True
+        if self.jumping:
+            if self.jumpdir:
+                self.y += 25
+                if self.y - self.jumpstart >= 105:
+                    self.jumpdir = False
+            else:
+                self.y -= 25
+                if self.y <= self.jumpstart:
+                    self.y = self.jumpstart
+                    self.jumpstart = 0
+                    self.jumping = False
+        return self.jumping
     def map_pos(self):
         return startx
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import math
 mariodir = 0
 mariorun = False
 mariojump = False
-jumpcnt = 0
 SCREENW = 1280; SCREENH = 800
 
 def handle_events():
@@ -22,48 +21,43 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 Play = False
             elif event.key == SDLK_LEFT:
-                mariodir = 2
+                if mariojump:
+                    mariodir = 3
+                else:
+                    mariodir = 2
             elif event.key == SDLK_RIGHT:
-                mariodir = 1
+                if mariojump:
+                    mariodir = 4
+                else:
+                    mariodir = 1
             elif event.key == SDLK_DOWN:
                 pass
             elif event.key == SDLK_UP:
                 pass
             elif event.key == SDLK_SPACE:
-                mariodir = 3
+                if mariodir == 2:
+                    mariodir = 3
+                    mariojump = True
+                elif mariodir == 1:
+                    mariodir = 4
+                    mariojump = True
+                else:
+                    mariodir = 5
+                    mariojump = True
                 pass
             elif event.key == SDLK_LSHIFT:
                 mario.frame = 0
                 mariorun = True
-            elif event.key == SDLK_SPACE:
-                mariojump = True
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_LEFT:
-                mariodir = 0
+                if not mariojump:
+                    mariodir = 0
             elif event.key == SDLK_RIGHT:
-                mariodir = 0
+                if not mariojump:
+                    mariodir = 0
             elif event.key == SDLK_LSHIFT:
                 mariorun = False
 
-def jump():
-    global mariojump
-    global mariodir
-    global jumpcnt
-
-    # 점프할때 마리오 시작위치를 넘겨주고 값이 변경되면 안됨
-    x1, y1 = mario.x, mario.y
-    x2, y2 = mario.x + 20, mario.y + 20
-    x3, y3 = mario.x + 40, mario.y
-
-    t = jumpcnt / 100
-    mario.x = (2 * t ** 2 - 3 * t + 1) * x1 + (-4 * t ** 2 + 4 * t) * x2 + (2 * t ** 2 - t) * x3
-    mario.y = (2 * t ** 2 - 3 * t + 1) * y1 + (-4 * t ** 2 + 4 * t) * y2 + (2 * t ** 2 - t) * y3
-
-    if jumpcnt == 100:
-        mariojump = False
-        mariodir = 0
-    else:
-        jumpcnt += 1
 
 open_canvas(SCREENW, SCREENH)
 
@@ -76,7 +70,6 @@ while Play:
     handle_events()
 
     clear_canvas()
-
 
     gamemap.draw(1)
     if mariodir == 1:
@@ -98,7 +91,21 @@ while Play:
         mario.left_move_draw()
 
     elif mariodir == 3:
-        jump()
+       if not mario.jump_left():
+           mariojump = False
+           mariodir = 2
+       mario.jump_draw()
+
+    elif mariodir == 4:
+        if not mario.jump_right():
+            mariojump = False
+            mariodir = 1
+        mario.jump_draw()
+
+    elif mariodir == 5:
+        if not mario.idle_jump():
+            mariojump = False
+            mariodir = 0
         mario.jump_draw()
 
     else:
