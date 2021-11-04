07519a04e8be9dbe949541c7b18fcb5a5b27743a
KimSeongtae <seongtae07@gmail.com>
2021-11-04 23:54:48 +0900

    맵 충돌체크

--- MakeMap.py
+++ MakeMap.py
@@ -6,11 +6,20 @@ width = round(SCREENW * ratio)
 
 class Map:
     image1 = None
+    tile1 = [[0 for col in range(10)] for row in range(255)]
     def __init__(self):
         if(Map.image1 == None):
             Map.image1 = load_image('Resource\Map1_Fix1.png')
             Map.image2 = load_image('Resource\Map2.png')
             Map.image3 = load_image('Resource\Map3.png')
+            Map.Basetile = load_image('Resource\Basetile.png')
+            for i in range(0, 10):
+                for j in range(0, 255):
+                    if i == 0 or i == 1:
+                        if j == 70 or 71 or 87 or 88 or 89 or 154 or 155:
+                            Map.tile1[j][i] = 0
+                        else:
+                            Map.tile1[j][i] = 1
         self.camerax = 0
         self.i = 0
         pass
@@ -18,7 +27,11 @@ class Map:
     def draw(self, i):
         if i == 1:
             self.i = 1
-            self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
+            # self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
+            for i in range(0, 10):
+                for j in range(0, 255):
+                    if Map.tile1[j][i] == 1:
+                        self.Basetile.clip_draw(0, 0, 15, 16, j * 15, i * 16, 50, 50)
         elif i == 2:
             self.i = 2
             self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
