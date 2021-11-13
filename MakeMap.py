e2ab8f83535ddf83dd09ac110f8af6c4bc1a7da1
KimSeongtae <seongtae07@gmail.com>
2021-11-13 23:27:35 +0900

    타일맵

--- MakeMap.py
+++ MakeMap.py
@@ -1,60 +1,177 @@
 from pico2d import *
+from Character import *
 
 SCREENW = 1280; SCREENH = 800
 ratio = 237 / SCREENH
 width = round(SCREENW * ratio)
 
 class Map:
-    image1 = None
-    tile1 = [[0 for col in range(10)] for row in range(255)]
+    image2 = None
+    tile1 = [[0 for col in range(16)] for row in range(255)]
+    num = 1
     def __init__(self):
-        if(Map.image1 == None):
-            Map.image1 = load_image('Resource\Map1_Fix1.png')
+        self.blocksize = 50
+        if(Map.image2 == None):
             Map.image2 = load_image('Resource\Map2.png')
             Map.image3 = load_image('Resource\Map3.png')
-            Map.Basetile = load_image('Resource\Basetile.png')
-            for i in range(0, 10):
+            Map.Basetile = load_image('Resource\Tile\Basetile.png')
+            Map.Skytile = load_image('Resource\Tile\sky(16x16).png')
+            Map.PipeHLtile = load_image('Resource\Tile\pipeHL.png')
+            Map.PipeHRtile = load_image('Resource\Tile\pipeHR.png')
+            Map.PipeBLtile = load_image('Resource\Tile\pipeBL.png')
+            Map.PipeBRtile = load_image('Resource\Tile\pipeBR.png')
+            Map.BigMountain = load_image('Resource\Tile\mountain(80x48).png')
+            Map.SmallMountain = load_image('Resource\Tile\mountain(48x32).png')
+            Map.Grass1 = load_image('Resource\Tile\grass(48x16).png')
+            Map.Grass3 = load_image('Resource\Tile\grass(80x16).png')
+            Map.Qtile = load_image('Resource\Tile\questiontile.png')
+            Map.Btile = load_image('Resource\Tile\Bricktile.png')
+            for i in range(0, 16):
                 for j in range(0, 255):
                     if i == 0 or i == 1:
                         if j == 70 or j == 71 or j == 87 or j == 88 or j == 89 or j == 154 or j == 155:
-                            Map.tile1[j][i] = 0
+                            Map.tile1[j][i] = 0  # Sky
                         else:
-                            Map.tile1[j][i] = 1
+                            Map.tile1[j][i] = 1  # Base
+                    elif i == 2:
+                        if j == 0 or j == 49:  # Big Mountain
+                            for a in range(j, j + 5):
+                                for b in range(i, i + 3):
+                                    Map.tile1[a][b] = -1
+                            Map.tile1[j][i] = 10
+
+                        elif j == 11 or j == 60 or j == 72:  # 3Grass
+                            for a in range(j, j + 5):
+                                Map.tile1[a][i] = -1
+                            Map.tile1[j][i] = 12
+
+                        elif j == 16 or j == 66:  # Small Mountain
+                            for a in range(j, j + 3):
+                                for b in range(i, i + 2):
+                                    Map.tile1[a][b] = -1
+                            Map.tile1[j][i] = 9
+
+                        elif j == 24:  # 1Grass
+                            for a in range(j, j + 3):
+                                Map.tile1[a][i] = -1
+                            Map.tile1[j][i] = 11
+
+                        elif j == 29 or j == 39 or j == 47 or j == 58:
+                            Map.tile1[j][i] = 2  # PipeBL
+                        elif j == 30 or j == 40 or j == 48 or j == 59:
+                            Map.tile1[j][i] = 3  # PipeBR
+                    elif i == 3:
+                        if j == 29:
+                            Map.tile1[j][i] = 4  # PipeHL
+                        elif j == 30:
+                            Map.tile1[j][i] = 5  # PipeHR
+                        elif j == 39 or j == 47 or j == 58:
+                            Map.tile1[j][i] = 2  # PipeBL
+                        elif j == 40 or j == 48 or j == 59:
+                            Map.tile1[j][i] = 3  # PipeBR
+                    elif i == 4:
+                        if j == 39:  # PipeHL
+                            Map.tile1[j][i] = 4
+                        elif j == 40:  # PipeHR
+                            Map.tile1[j][i] = 5
+                        elif j == 47 or j == 58:
+                            Map.tile1[j][i] = 2
+                        elif j == 48 or j == 59:
+                            Map.tile1[j][i] = 3
+                    elif i == 5:
+                        if j == 16 or j == 22 or j == 24:  # questiontile
+                            Map.tile1[j][i] = 6
+                        elif j == 21 or j == 23 or j == 25:  # brick
+                            Map.tile1[j][i] = 7
+                        elif j == 47 or j == 58:
+                            Map.tile1[j][i] = 4
+                        elif j == 48 or j == 59:
+                            Map.tile1[j][i] = 5
+                    elif i == 9:
+                        if j == 23:
+                            Map.tile1[j][i] = 6
         self.camerax = 0
-        Map.i = 0
         pass
 
     def draw(self):
-        if Map.i == 1:
-            self.i = 1
-            self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
-            for i in range(0, 10):
+        if Map.num == 1:
+            self.num = 1
+            for i in range(0, 16):
                 for j in range(0, 255):
-                    if Map.tile1[j][i] == 1:
-                        self.Basetile.clip_draw(0, 0, 15, 16, j * 50, i * 50 + 25, 50, 50)
-        elif Map.i == 2:
-            self.i = 2
-            self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
+                    if Map.tile1[j][i] == 1 and i != 1:
+                        self.Basetile.clip_draw(0, 0, 16, 32,
+                                                (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                i * self.blocksize + self.blocksize, self.blocksize, self.blocksize * 2)
+                    elif Map.tile1[j][i] == 2:
+                        self.PipeBLtile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 3:
+                        self.PipeBRtile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 4:
+                        self.PipeHLtile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 5:
+                        self.PipeHRtile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 6:
+                        self.Qtile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 7:
+                        self.Btile.clip_draw(0, 0, 16, 16,
+                                                  (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                                  i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                                  self.blocksize)
+                    elif Map.tile1[j][i] == 9:
+                        self.SmallMountain.clip_draw(0, 0, 48, 32,
+                                                   (j * self.blocksize) + ((self.blocksize * 3) // 2) - self.camerax,
+                                                   i * self.blocksize + ((self.blocksize * 2) // 2), self.blocksize * 3,
+                                                   self.blocksize * 2)
+                    elif Map.tile1[j][i] == 10:
+                        self.BigMountain.clip_draw(0, 0, 80, 48,
+                                                   (j * self.blocksize) + ((self.blocksize * 5) // 2) - self.camerax,
+                                                   i * self.blocksize + ((self.blocksize * 3) // 2), self.blocksize * 5,
+                                                   self.blocksize * 3)
+                    elif Map.tile1[j][i] == 11:
+                        self.Grass1.clip_draw(0, 0, 48, 16,
+                                                   (j * self.blocksize) + ((self.blocksize * 3) // 2) - self.camerax,
+                                                   i * self.blocksize + ((self.blocksize) // 2), self.blocksize * 3,
+                                                   self.blocksize)
+                    elif Map.tile1[j][i] == 12:
+                        self.Grass3.clip_draw(0, 0, 80, 16,
+                                                   (j * self.blocksize) + ((self.blocksize * 5) // 2) - self.camerax,
+                                                   i * self.blocksize + ((self.blocksize) // 2), self.blocksize * 5,
+                                                   self.blocksize)
+                    elif Map.tile1[j][i] == 0:
+                        self.Skytile.clip_draw(0, 0, 16, 16,
+                                               (j * self.blocksize) + (self.blocksize // 2) - self.camerax,
+                                               i * self.blocksize + (self.blocksize // 2), self.blocksize,
+                                               self.blocksize)
+
+        elif Map.num == 2:
+            self.num = 2
+            self.image2.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
             pass
         else:
-            self.i = 3
-            self.image1.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
+            self.num = 3
+            self.image3.clip_draw(self.camerax, 0, width, 237, SCREENW / 2, SCREENH / 2, SCREENW, SCREENH)
             pass
 
-    def get_camera(self, x, startx):
-        if self.i == 1:
-            if x >= (SCREENW / 2) and (SCREENW / 2 + startx + x) <= 3375:
-                self.camerax = startx
-
-        elif self.i == 2:
-
-            if x >= (SCREENW / 2) and (SCREENW / 2 + startx + x) <= 2624:
-                self.camerax = startx
-
-        else:
-            if x >= (SCREENW / 2) and (SCREENW / 2 + startx + x) <= 2560:
-                self.camerax = startx
-
-    def update(self):
+    def update(self, mario):
+        if mario.get_marioPos() == SCREENW - 300:
+            self.camerax = int(mario.get_MapX())
+        elif mario.get_marioPos() == 200:
+            self.camerax = int(mario.get_MapX())
         pass
+
 
